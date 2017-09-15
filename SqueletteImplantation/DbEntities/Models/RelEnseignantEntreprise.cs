e4fa91141b3f769055c3fda553c5bc449899246e
Jean-Denis Thiboutot <jeandenisthiboutot@yahoo.ca>
2017-09-15 09:14:30 -0400

    Préparation au merge

---
+++ SqueletteImplantation/DbEntities/Models/RelEnseignantEntreprise.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SqueletteImplantation.DbEntities.Models
+{
+    public class RelEnseignantEntreprise
+    {
+        //Clé étrangère Enseignant:
+        public int NoEnseignant { get; set; }
+        public Professeur Professeur { get; set; }
+        //Clé étrangère Entreprise:
+        public int NoEntreprise { get; set; }
+        public Entreprise Entreprise { get; set; }
+
+    }
+}
