93f671ac25dac7b15a90a1c9472daec812eb4c7b
Romy Steve <romyngangoue@gmail.com>
2017-10-05 02:30:43 -0400

    modifcomponentAccueilEnseignant

--- SqueletteImplantation/wwwroot/app/pageAccueilEnseignant.component.ts
+++ SqueletteImplantation/wwwroot/app/pageAccueilEnseignant.component.ts
@@ -7,63 +7,73 @@ import { Component } from '@angular/core';
 
 import {  Router, RouterModule, Routes}   from '@angular/router';
 
-import { PageRechercheEtudiantComponent } from "./pageRechercheEtudiant.component";
+
 
 
 @Component({
     selector: 'accueil_enseignant',
 
-    templateUrl:`./../html/AccueilEnseignant.html`,
-    styleUrls:[`./../css/accueil_enseignant.css`],
+    templateUrl: `./../html/AccueilEnseignant.html`,
+    styleUrls: [`./../css/accueil_enseignant.css`],
 })
 
-export class pageAccueilEnseignantComponent { 
+export class pageAccueilEnseignantComponent
+{
     entreprises: Entreprise[];
+    annees: string;
+    Recherche: string;
+    TAnnees: String[];
+    constructor(private http: Http, private router: Router)
+    {
+        this.annees = "";
+        this.Recherche = "";
+        this.getEntreprise("", "");
+        this.RemplirCombo();
+    }
 
-    constructor(private http: Http, private router: Router){
-  
+    Deconnexion() {
+        localStorage.removeItem('currentUser');
+        this.router.navigate(['/Login']);
+    }
+    RemplirCombo() {
+        this.http.get("api/Entreprise/RemplirCombo").subscribe(
+            donnees => {
+                this.TAnnees = donnees.json() as String[]
+                console.log(this.TAnnees);
+            });
+        } 
+    getEntreprise(Recherche: string, annees: string) {
+        let url: string;
+        if ((Recherche == "" && annees == "")) {
+            url = "api/Entreprise/annees";
+        }
+        else {
+            if (Recherche != "" && annees == "") {
+                url = " api/Entreprise/RechercheSansAnnee/" + Recherche;
+            }
+            else {
+                if (Recherche == "" && annees != "") {
+                    url = "api/Entreprise/RechercheAnnee/" + annees
+                }
+                else {
+                    url = "api/Entreprise/" + annees + "/" + Recherche;
+                }
+
+            }
         }
 
-        Deconnexion(){
-            localStorage.removeItem('currentUser');
-            this.router.navigate(['/Login']);
-        }
-        getEntreprise(annee:string){
+        this.http.get(url).subscribe(
+            donnees => {
+                this.entreprises = donnees.json() as Entreprise[]
+                console.log(this.entreprises);
+            }
+        );
 
-            this.http.get("api/Entreprise/" + annee).subscribe(
-                donnees => {
-                this.entreprises = donnees.json() as Entreprise[]               
-                }    
-            );
-        }
-        
-        
-
-       /* const appRoutes: Routes=[
-
-            {
-                path: 'recherche-etudiant', 
-                component: PageRechercheEtudiantComponent 
-              },
-
-        ]*/
+    }
+}
 
 
 
 
-
-      }
-
-      //export const routing = RouterModule.forRoot(appRoutes);
-     /* @NgModule({
-        imports: [
-          RouterModule.forRoot(
-            appRoutes,
-            { enableTracing: true } // <-- debugging purposes only
-          )
-          // other imports here
-        ],
-        ...
-      })*/
 
       
