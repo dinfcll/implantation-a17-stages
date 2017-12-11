9fbde520fa908a62fc42c0f797ac6d731bf81a46
Jean-Denis Thiboutot <jeandenisthiboutot@yahoo.ca>
2017-12-11 13:57:37 -0500

    aa

--- SqueletteImplantation/wwwroot/app/app.module.ts
+++ SqueletteImplantation/wwwroot/app/app.module.ts
@@ -65,7 +65,7 @@ const appRoutes: Routes=[
   },
     {
         path: 'detailEtudiant/:noDa', 
-        canActivate: [ AuthguardGuard ],
+        canActivate: [ AuthguardGuardens ],
         component:  PageDetailEtudiantComponent
     },
     {
